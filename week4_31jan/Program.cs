17ead3712518e8eff06552dd7ed5ad7e815e4ac2
visualstudio <visualstudio@230-PC28>
2019-01-31 15:20:43 -0500

    week4

--- week4_31jan/Program.cs
+++ week4_31jan/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace week4_31jan
 {
@@ -6,7 +10,41 @@ namespace week4_31jan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+        class Village
+        {
+            public static int numberofvillages = 0;
+            public Village nextvillage;
+            public Village previousvillage;
+            public string Villagename;
+            public bool isAstrildeHere = false;
+            public Village()
+            { Village.numberofvillages++; }
+        }
+        class Countryside
+        {
+            public Village Maple;
+
+            public Village Toronto;
+            public Village Ajax;
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.Villagename = "Maple";
+                Maple.previousvillage = null;
+                Toronto = new Village();
+                Toronto.Villagename = "Toronto";
+                Toronto.previousvillage = null;
+                Ajax = new Village();
+                Ajax.Villagename = "Ajax";
+                Ajax.nextvillage = null;
+                Ajax.previousvillage = Toronto;
+                Ajax.isAstrildeHere = true;
+
+
+            }
         }
     }
 }
+
