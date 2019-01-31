5921527dc3ed5c9f9122ff9f60e7b031b3c56df1
visualstudio <visualstudio@230-PC28>
2019-01-31 16:57:04 -0500

    last commit

--- week4_31jan/Program.cs
+++ week4_31jan/Program.cs
@@ -4,47 +4,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace week4_31jan
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Countryside Ontario = new Countryside();
+            Ontario.run();
 
-        }
-        class Village
-        {
-            public static int numberofvillages = 0;
-            public Village nextvillage;
-            public Village previousvillage;
-            public string Villagename;
-            public bool isAstrildeHere = false;
-            public Village()
-            { Village.numberofvillages++; }
-        }
-        class Countryside
-        {
-            public Village Maple;
-
-            public Village Toronto;
-            public Village Ajax;
-            public void MapInitializer()
-            {
-                Maple = new Village();
-                Maple.Villagename = "Maple";
-                Maple.previousvillage = null;
-                Toronto = new Village();
-                Toronto.Villagename = "Toronto";
-                Toronto.previousvillage = null;
-                Ajax = new Village();
-                Ajax.Villagename = "Ajax";
-                Ajax.nextvillage = null;
-                Ajax.previousvillage = Toronto;
-                Ajax.isAstrildeHere = true;
-
-
-            }
         }
     }
-}
 
+    class LearningExample
+    {
+        public void run()
+        {
+            Village Toronto;
+            Village a;
+            Village b;
+            Toronto = new Village();
+            a = Toronto;
+
+            Toronto = new Village();
+            b = Toronto;
+
+            if (a == b) { Console.WriteLine("same"); }
+            else
+            { Console.WriteLine("different obj refs"); }
+        }
+    }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+    }
+
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+        public Village Head;
+        public Village Tail;
+        public Village Current;
+
+        public void run()
+        {
+            this.MapInitializer();
+            this.LookForAstrilde();
+            Console.WriteLine("Hugi found Astrilde in " + Current.VillageName);
+        }
+
+        public void MapInitializer()
+        {
+            Ajax = new Village();
+            Toronto = new Village();
+            Maple = new Village();
+
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Maple.isAstrildeHere = true;
+
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+        }
+        public void LookForAstrilde()
+        {
+
+            Current = Maple;
+            while (Current.nextVillage != null)
+            {
+                if (Current.isAstrildeHere)
+                {
+                    Console.WriteLine("Found Astrilde");
+                    return;
+
+                }
+                else
+                {
+                    Current = Current.nextVillage;
+
+                }
+            }
+        }
+
+    }
+}
